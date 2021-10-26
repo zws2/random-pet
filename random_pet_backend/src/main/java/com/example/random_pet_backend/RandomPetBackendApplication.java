ee90c612825edb1e7ca9a747dc09269aa41b62a7
Zachary Snyder <snyderzw@hotmail.com>
2021-10-26 10:49:12 -0500

    added frontend and backend projects

---
+++ random_pet_backend/src/main/java/com/example/random_pet_backend/RandomPetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.random_pet_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomPetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomPetBackendApplication.class, args);
+	}
+
+}
---
+++ random_pet_backend/src/test/java/com/example/random_pet_backend/RandomPetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.random_pet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomPetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
