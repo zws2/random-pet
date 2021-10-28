6cd5f958dac86bf143c92c14e88ccf266a132a25
Zachary Snyder <snyderzw@hotmail.com>
2021-10-28 09:42:57 -0500

    WIP for retrieving image

--- random_pet_frontend/src/component/general/WelcomeComponent.jsx
+++ random_pet_frontend/src/component/general/WelcomeComponent.jsx
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import PetDataService from '../../service/PetDataService';
 
 class WelcomeComponent extends Component {
     constructor(props) {
         super(props)
-        this.state={
-            name: this.props.name
+        this.state = {
+            pets: []
         }
+        this.refreshPetRegistry.refreshPetRegistry.bind(this)
     }
+
+    refreshPetRegistry() {
+        var image
+        PetDataService.retrieveAllPets()
+        .then(
+            response => {
+                image = response.data
+            }
+        )
+        console.log(image)
+    }
+
     render() {
+
         return(
             <div className="image_container">
                 <h1 style={{textAlign: "center"}}>Cash Money</h1>
                 <div class="imgbox">
-                    <img class="center-fit" src='https://i.imgur.com/sFq0wAC.jpeg'></img>
+{/*                     <img class="center-fit" src='https://i.imgur.com/sFq0wAC.jpeg'/> */}
+{/*                     <img class="img-circle img-responsive center-fit" src={"data:image/png;base64," + image} /> */}
 {/*                     https://i.imgur.com/QBSDdJJ.jpeg */}
                 </div>
                 <p style={{textAlign:"center"}}>this birb is wearing a money hat</p>
